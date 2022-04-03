18e35c1de1dfee0837c3939f12ef444b5df8a70a
delameter <el.nprotect@gmail.com>
2022-04-03 08:11:09 +0300

    global refactoring autoformat feature

--- src/pytermor/util.py
+++ src/pytermor/util.py
@@ -6,35 +6,35 @@ from __future__ import annotations
 
 import re
 from functools import reduce
-from typing import Callable, AnyStr, Type
+from typing import Callable, AnyStr, Type, Generic
 
 
-class StringFilter:
-    def __init__(self, fn: Callable):
+class StringFilter(Generic[AnyStr]):
+    def __init__(self, fn: Callable[[AnyStr], AnyStr]):
         self._fn = fn
 
-    def __call__(self, s: str):
-        return self.invoke(s)
-
-    def invoke(self, s: str):
+    def __call__(self, s: AnyStr) -> AnyStr:
         return self._fn(s)
 
+    def invoke(self, s: AnyStr) -> AnyStr:
+        return self(s)
 
-class ReplaceSGR(StringFilter):
+
+class ReplaceSGR(StringFilter[str]):
     """Find all SGR seqs (e.g. '\\e[1;4m') and replace with given string.
     More specific version of ReplaceCSI()."""
-    def __init__(self, repl: AnyStr = ''):
+    def __init__(self, repl: str = ''):
         super().__init__(lambda s: re.sub(r'\033\[([0-9;]*)(m)', repl, s))
 
 
-class ReplaceCSI(StringFilter):
+class ReplaceCSI(StringFilter[str]):
     """Find all CSI seqs (e.g. '\\e[*') and replace with given string.
-    Less specific version of ReplaceSGR(), as CSI consists of SGR and many other seq sub-types."""
-    def __init__(self, repl: AnyStr = ''):
+    Less specific version of ReplaceSGR(), as CSI consists of SGR and many other seq subtypes."""
+    def __init__(self, repl: str = ''):
         super().__init__(lambda s: re.sub(r'\033\[([0-9;:<=>?]*)([@A-Za-z])', repl, s))
 
 
-class ReplaceNonAsciiBytes(StringFilter):
+class ReplaceNonAsciiBytes(StringFilter[bytes]):
     """Keep [0x00 - 0x7f], replace if greater than 0x7f."""
     def __init__(self, repl: bytes = b'?'):
         super().__init__(lambda s: re.sub(b'[\x80-\xff]', repl, s))
