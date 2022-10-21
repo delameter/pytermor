ae611c5e4f64e175239d677b7c7f9914d466fe4c
delameter <0.delameter@gmail.com>
2022-10-21 13:46:39 +0300

    🔧 MAINTAIN: dev artifacts

--- pytermor/__init__.py
+++ pytermor/__init__.py
@@ -6,6 +6,7 @@ from .ansi import IntCodes, Seqs, Spans, SequenceSGR, SequenceOSC, Span, NOOP_SE
 from .color import Colors, Color, ColorIndexed16, ColorIndexed256, ColorRGB, NOOP_COLOR
 from .text import render, Styles, Style, Text, SgrRenderer, RendererManager, NOOP_STYLE
 from ._version import __version__
+from .common import Registry
 
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
