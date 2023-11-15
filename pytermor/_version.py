e6066ed789dfc35bd4453b6f63b0d64d649cd0ec
delameter <0.delameter@gmail.com>
2023-11-15 04:31:58 +0300

    🐞 FIX: version

--- pytermor/_version.py
+++ pytermor/_version.py
@@ -3,5 +3,5 @@
 #  (c) 2022-2023. A. Shavykin <0.delameter@gmail.com>
 #  Licensed under GNU Lesser General Public License v3.0
 # -----------------------------------------------------------------------------
-__version__ = '2.108.0'
-__updated__ = "2023-11-13 01:25:45+03:00"
+__version__ = '2.108.0.dev0'
+__updated__ = "2023-11-15 04:31:38+03:00"
